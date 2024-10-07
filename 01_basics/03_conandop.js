5a0157d5a9c980ce97de40a2b9906040d4de809d
fantasticIshan <162364478+fantasticIshan@users.noreply.github.com>
2024-10-07 09:49:07 +0000

    Conversion of data type

--- 01_basics/03_conandop.js
+++ 01_basics/03_conandop.js
@@ -1,7 +1,27 @@
-let score = "33"
+// let score = undefined
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" =>33
+// "33abc" =>Nan
+// ture =>1; fales=> 0 
+// let isLoggedIn ="ishan"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//1=>true; 0=> false
+// "" => false
+// "ishan" => true
+
+let someNumber = 33
+let stringNumber  =  String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
