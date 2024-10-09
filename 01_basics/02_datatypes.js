2880e8b0e73434f0158b42577658eb8fdf6d1d62
fantasticIshan <162364478+fantasticIshan@users.noreply.github.com>
2024-10-10 00:38:43 +0530

    Update 02_datatypes.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,27 +1,29 @@
-"use strict"  // treat all JS  ocde as newer version
+"use strict"; // treat all JS code as newer version
 
-// alert(3+3) we are using nodejs , not browser
-console.log(3+3);  console.log("Ishan"); 
-//code readabilty is important.
+// alert( 3 + 3) // we are using nodejs, not browser
 
-let name = "ishan"
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
 let age = 18
 let isLoggedIn = false
 let state;
 
-//number => 2 to power 53
-//bigint
-//string=>""
-//boolean => true/false
-//null =>standalone value
-//undefined =>
-// symbol =>unique
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
 
 
-//object
+// object
 
-// type of object is null
-
-
-console.log(type age);
-
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
